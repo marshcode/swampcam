5d70d132b8120a4585c6d41b27ab5eb74a432b9f
davmarsh <david.the.marsh@gmail.com>
2022-07-07 22:06:35 -0400

    stitches

--- bin/camera_bank_test.py
+++ bin/camera_bank_test.py
@@ -1,14 +1,16 @@
 import traceback
+import datetime
 
-from swampcam.camera_banks import camera_bank
+from swampcam.camera_banks import camera_bank as camera_bank_mod
 from swampcam.camera_banks.cv2_bank import CV2VideoCaptureBank
 from swampcam.camera_banks.thread_bank import ThreadBank
 
 from swampcam.displays import multi_display
 
 from swampcam.pipeline import resize
+from swampcam.pipeline import stitch
 
-camera_bank = camera_bank.CameraBank()
+camera_bank = camera_bank_mod.CameraBank()
 multi_display = multi_display.MultiDisplay()
 
 cv2_bank = CV2VideoCaptureBank(camera_bank)
@@ -18,16 +20,23 @@ cv2_bank_thread = ThreadBank(cv2_bank, delay_ms=10)
 
 cv2_bank_thread.start()
 
-while True:
-    try:
+stitcher = stitch.ImageStitcher()
+
+try:
+    while True:
         captures = camera_bank.get_captures()
         captures = resize.resize(400, 300, captures)
-        key = multi_display.display(captures)
+        if not captures:
+            continue
+
+        combined = stitcher.combine(captures)
+        key = multi_display.display({'combined': camera_bank_mod.CameraCapture(
+            combined, datetime.datetime.now()
+        )})
         if key == ord('q'):
             break
-    except Exception as e:
-        print(traceback.format_exc())
-        break
+except Exception as e:
+    print(traceback.format_exc())
 
 cv2_bank_thread.stop()
 cv2_bank.destroy()
---
+++ src/swampcam/pipeline/stitch.py
@@ -0,0 +1,50 @@
+import numpy as np
+import math
+
+class ImageStitcher(object):
+    def __init__(self):
+        pass
+
+    def combine(self, captures):
+
+        if len(captures) == 0:
+            return None
+
+        capture_names = list(captures.keys())
+
+        chunked_list = list()
+        chunk_size = 2 if len(captures) < 2 else int(math.sqrt(len(capture_names)))
+        for i in range(0, len(capture_names), chunk_size):
+            chunked_list.append(capture_names[i:i + chunk_size])
+
+        canvas_width = 0
+        canvas_height = 0
+        for row in chunked_list:
+            row_width = 0
+            max_height = 0
+            for capture_name in row:
+                capture = captures[capture_name]
+                row_width += capture.image.shape[1]
+                max_height = max(max_height, capture.image.shape[0])
+
+            canvas_width = max(row_width, canvas_width)
+            canvas_height += max_height
+
+        canvas = np.zeros((canvas_height, canvas_width, 3), np.uint8)
+
+        h_start = 0
+        for row in chunked_list:
+            w_start = 0
+            max_height = 0
+            for capture_name in row:
+                image = captures[capture_name].image
+                w_end = w_start + image.shape[1]
+                h_end = h_start + image.shape[0]
+                max_height = max(max_height, image.shape[0])
+
+                canvas[h_start:h_end, w_start:w_end, :3] = image
+
+                w_start = w_end
+            h_start += max_height
+
+        return canvas
