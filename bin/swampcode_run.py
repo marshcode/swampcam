3bd7ba42da3fd56a52586a43f27a5692b51110a7
davmarsh <david.the.marsh@gmail.com>
2022-07-10 12:12:49 -0400

    now it runs locally

--- bin/swampcode_run.py
+++ bin/swampcode_run.py
@@ -3,6 +3,7 @@ import time
 
 import bin_factory
 
+from swampcam.camera_banks import camera_bank
 from swampcam.displays import multi_display
 from swampcam.web import factory as web_factory
 
@@ -18,9 +19,15 @@ threading.Thread(target=add_delay, daemon=True).start()
 
 
 #######################
+#Go Between
+#######################
+web_buffer = camera_bank.CameraBank()
+web_buffer_key = 'web_buffer'
+
+#######################
 #Web
 #######################
-flask_app = web_factory.create()
+flask_app = web_factory.create(web_buffer, web_buffer_key)
 web_thread = threading.Thread(name="flask", daemon=True, target=lambda: flask_app.run(
     host='0.0.0.0', port=4785, debug=True, threaded=True, use_reloader=False
 ))
@@ -34,6 +41,11 @@ camera_interface.start()
 motion_runner = bin_factory.create_motion_runner(camera_interface.camera_bank)
 for captures, signal_up in motion_runner():
     key = multi_display.display(captures)
+
+    if len(captures) > 0:
+        capture = list(captures.values())[0]
+        web_buffer.add_capture(web_buffer_key, capture.image, capture.timestamp)
+
     if key == ord('q'):
         break
 
--- src/swampcam/web/factory.py
+++ src/swampcam/web/factory.py
@@ -3,11 +3,34 @@ import os
 from swampcam.web import templates
 
 import flask
+import cv2
 
-def create():
+def create(camera_bank, capture_key):
     template_dir = os.path.dirname(os.path.abspath(templates.__file__))
     app = flask.Flask("admin", template_folder=template_dir)
 
+    def generate():
+        while True:
+            capture = camera_bank.get_captures().get(capture_key)
+            # wait until the lock is acquired
+            if not capture or capture.image is None:
+                continue
+
+            # encode the frame in JPEG format
+            (flag, encodedImage) = cv2.imencode(".jpg", capture.image)
+            # ensure the frame was successfully encoded
+            if not flag:
+                continue
+            # yield the output frame in the byte format
+            yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+                   bytearray(encodedImage) + b'\r\n')
+
+
+    @app.route("/video_feed")
+    def video_feed():
+        # return the response generated along with the specific media
+        # type (mime type)
+        return flask.Response(generate(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
     @app.route("/")
     def index():
